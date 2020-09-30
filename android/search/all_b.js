549b8e4f7e85b1af092dc7dad6fe16dbdd5c21f4
Brian Lutz <blutz@connectify.me>
2020-09-30 12:41:57 -0400

    fix casing of oauth function names

--- android/search/all_b.js
+++ android/search/all_b.js
@@ -22,7 +22,7 @@ var searchData=
   ['loghandler',['LogHandler',['../classcom_1_1speedify_1_1speedifysdk_1_1_logging_1_1_log_handler.html',1,'com.speedify.speedifysdk.Logging.LogHandler'],['../classcom_1_1speedify_1_1speedifysdk_1_1_logging_1_1_log_handler.html#a4b284b454b8d93490f5b114cdb9b5b94',1,'com.speedify.speedifysdk.Logging.LogHandler.LogHandler()']]],
   ['login',['login',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a127d9ec58ada8901c5ad42f2b9cc0377',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['loginautoaccount',['loginAutoAccount',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a8c03b7892aac0bb58ef0adad37b81f6c',1,'com::speedify::speedifysdk::SpeedifySDK']]],
-  ['loginoauth',['loginOauth',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a064d56a2aeb14d6aef2380d525cd5a91',1,'com::speedify::speedifysdk::SpeedifySDK']]],
+  ['loginoauth',['loginOAuth',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#aa1f2563b2c55b0ce5c534e285664b7a6',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['loglevel',['LogLevel',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_log_level.html',1,'com.speedify.speedifysdk.Types.LogLevel'],['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_daemon_log_settings.html#a6847126f8c7fbdd53477e913f9eb6e86',1,'com.speedify.speedifysdk.Types.DaemonLogSettings.logLevel()']]],
   ['logout',['logout',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#ab7f35ca0ad17c0fc03b8a04448198522',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['loopback',['LOOPBACK',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_network_type.html#a3f911e238d93ba55511af7b472750ad5',1,'com::speedify::speedifysdk::Types::NetworkType']]],
