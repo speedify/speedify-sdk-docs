b4ef630eb115cb2f4936598f6fe70fd67226b009
Brian Lutz <blutz@connectify.me>
2022-07-19 08:53:21 -0400

    speedify 12.3 sdk docs

--- android/search/all_7.js
+++ android/search/all_7.js
@@ -3,6 +3,7 @@ var searchData=
   ['handleadapteroverlimit',['handleAdapterOverlimit',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a31fd2e08efad3a9d2da0f51b4b9d9a78',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['handlenewadapter',['handleNewAdapter',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a5f4ad6e4986f05241979216ed011f933',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['handlerstats',['HandlerStats',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_handler_stats.html',1,'com.speedify.speedifysdk.Types.HandlerStats'],['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_session_stats.html#ad831df42df78641d082abd547dbc5a72',1,'com.speedify.speedifysdk.Types.SessionStats.handlerStats()']]],
+  ['has',['has',['../classcom_1_1speedify_1_1speedifysdk_1_1_preferences.html#a629acb98bd94a118e207441ded2b32d7',1,'com::speedify::speedifysdk::Preferences']]],
   ['hasvpnpermission',['hasVPNPermission',['../classcom_1_1speedify_1_1speedifysdk_1_1_speedify_s_d_k.html#a28168c54f50eb91a95cd551b51cf928a',1,'com::speedify::speedifysdk::SpeedifySDK']]],
   ['headercompression',['headerCompression',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_settings.html#abd1b8f25d1a2e298fa39276f29277c9f',1,'com::speedify::speedifysdk::Types::Settings']]],
   ['health',['health',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_stream_stats.html#a5380e82b22b64d03c1dd9e1e37fd67f1',1,'com::speedify::speedifysdk::Types::StreamStats']]],
