d93bea9144d9761e54436cd64d0c9c69add10247
Brian Lutz <blutz@connectify.me>
2025-12-08 09:26:04 -0500

    speedify 16.1.1 sdk docs

--- android/search/variables_e.js
+++ android/search/variables_e.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['off_0',['OFF',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_firewall_mode.html#aba437f58ad654d395b18d209a1a8c21c',1,'com::speedify::speedifysdk::Types::FirewallMode']]],
-  ['ok_1',['OK',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_error_type.html#a509f04652b5e1b2d2b9e6bc121a87e50',1,'com::speedify::speedifysdk::Types::ErrorType']]],
-  ['overflowthreshold_2',['overflowThreshold',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_settings.html#adf79bb31096ffa7c62c9b129c24d40a9',1,'com::speedify::speedifysdk::Types::Settings']]],
-  ['overlimit_3',['OVERLIMIT',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_state.html#ae31590673dc90d41b7a9345b1d03dd88',1,'com::speedify::speedifysdk::Types::State']]],
-  ['overlimitratelimit_4',['overlimitRatelimit',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_adapter_usage_data.html#a5f7c13433b2e69ff8ece20e7cac42264',1,'com::speedify::speedifysdk::Types::AdapterUsageData']]],
-  ['overquota_5',['OVERQUOTA',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_disconnect_reason.html#ab6cd9555b63c5673d67da5397fe929ed',1,'com::speedify::speedifysdk::Types::DisconnectReason']]]
+  ['offline_1',['offline',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_adapter_data.html#a9454a8f6cb973b8a7e61a2aa5529f841',1,'com::speedify::speedifysdk::Types::AdapterData']]],
+  ['ok_2',['OK',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_error_type.html#a509f04652b5e1b2d2b9e6bc121a87e50',1,'com::speedify::speedifysdk::Types::ErrorType']]],
+  ['overflowthreshold_3',['overflowThreshold',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_settings.html#adf79bb31096ffa7c62c9b129c24d40a9',1,'com::speedify::speedifysdk::Types::Settings']]],
+  ['overlimit_4',['OVERLIMIT',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_state.html#ae31590673dc90d41b7a9345b1d03dd88',1,'com::speedify::speedifysdk::Types::State']]],
+  ['overlimitratelimit_5',['overlimitRatelimit',['../classcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_adapter_usage_data.html#a5f7c13433b2e69ff8ece20e7cac42264',1,'com::speedify::speedifysdk::Types::AdapterUsageData']]],
+  ['overquota_6',['OVERQUOTA',['../enumcom_1_1speedify_1_1speedifysdk_1_1_types_1_1_disconnect_reason.html#ab6cd9555b63c5673d67da5397fe929ed',1,'com::speedify::speedifysdk::Types::DisconnectReason']]]
 ];
--- desktop/search/all_18.js
+++ desktop/search/all_18.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['overflowthreshold_0',['overflowThreshold',['../struct_speedify_s_d_k___settings.html#a197cf0fd1972391e8f79ee2a044c5926',1,'SpeedifySDK_Settings']]],
-  ['overlimitratelimit_1',['overlimitRatelimit',['../struct_speedify_s_d_k___adapter_usage_data.html#a98ebbd6a61afb1fe9e83a8a2bce277d2',1,'SpeedifySDK_AdapterUsageData']]],
-  ['overview_2',['Overview',['../networksharing.html#autotoc_md13',1,'']]],
-  ['overview_2emd_3',['overview.md',['../overview_8md.html',1,'']]]
+  ['offline_0',['offline',['../struct_speedify_s_d_k___adapter_data.html#a4b94bb87d2266ef6850abd51f1d26e17',1,'SpeedifySDK_AdapterData']]],
+  ['overflowthreshold_1',['overflowThreshold',['../struct_speedify_s_d_k___settings.html#a197cf0fd1972391e8f79ee2a044c5926',1,'SpeedifySDK_Settings']]],
+  ['overlimitratelimit_2',['overlimitRatelimit',['../struct_speedify_s_d_k___adapter_usage_data.html#a98ebbd6a61afb1fe9e83a8a2bce277d2',1,'SpeedifySDK_AdapterUsageData']]],
+  ['overview_3',['Overview',['../networksharing.html#autotoc_md13',1,'']]],
+  ['overview_2emd_4',['overview.md',['../overview_8md.html',1,'']]]
 ];
